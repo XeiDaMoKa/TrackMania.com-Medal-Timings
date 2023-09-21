e2a074c6deb39a8c6736fe3706a05003c001530f
XeiDaMoKa <xeidamoka@hotmail.com>
2023-09-21 01:59:48 +0100

    updated

--- content.js
+++ content.js
@@ -1,4 +1,3 @@
-// console log with $$
 const $$ = function(...args) {
     for (let arg of args) {
         if (arg instanceof jQuery) {
@@ -8,12 +7,6 @@ const $$ = function(...args) {
         }
     }
 };
-// Find Tracks
-const OCtracks = $('.container.mt-5.d-block.d-xxl-none .col');
-$$(OCtracks);
-const COTDtracks = $('.container.my-2 .col.p-1');
-$$(COTDtracks);
-const ALLtracks = OCtracks.add(COTDtracks);
 
 // Append Buttons
 fetch(chrome.runtime.getURL("content.html"))
@@ -23,40 +16,26 @@ fetch(chrome.runtime.getURL("content.html"))
         $$("Buttons Appended");
     });
 
-// Fetch URL of All Tracks
-ALLtracks.each(function() {
-    const trackElement = $(this);
+const processTrackElement = function(trackElement) {
     const trackHref = trackElement.find('a').attr('href');
     $$(`Fetching HREF`);
-
-    // Fetch Content of URL
     fetch(trackHref)
         .then(response => response.text())
         .then(html => {
             const trackPage = $(html);
-
-            // Get Medals List
             const medalsList = trackPage.find('.list-unstyled.tm-map-score');
-
-            // Create Medals List DIV, Append Medals List to DIV and Append DIV to IMG
             const medalsDiv = $('<div>', {
                 class: 'tm-map-card-official-footer medals-div'
             });
             medalsDiv.append(medalsList);
             trackElement.find('a.tm-map-card-official-link img').after(medalsDiv);
-
-            // Get PB, Assign Custom Class & Append to Footer
             const personalBest = trackPage.find('p.tm-map-score').addClass('personal-best');
             trackElement.find('.tm-map-card-official-footer.d-flex.flex-column > div').eq(1).html(personalBest);
-
-            // Extract the PB time and Author time.
             let timePB = personalBest.text().trim().split("\n").pop().trim();
             if (timePB === "--:--.---") {
                 timePB = "00:00.000";
             }
             const authorTime = medalsList.find('li').first().text().trim().split("\n").pop().trim();
-
-            // Compute the time difference.
             const solveTIME = (time1, time2) => {
                 const [minutes1, seconds1] = time1.split(':').map(parseFloat);
                 const [minutes2, seconds2] = time2.split(':').map(parseFloat);
@@ -64,53 +43,39 @@ ALLtracks.each(function() {
                 const absoluteDiffInSeconds = Math.abs(rawDiffInSeconds);
                 const diffMinutes = Math.floor(absoluteDiffInSeconds / 60);
                 const diffSeconds = (absoluteDiffInSeconds % 60).toFixed(3);
-
                 let prefix = "";
                 if (rawDiffInSeconds > 0 || time1 === "00:00.000") {
                     prefix = "-";
                 } else {
                     prefix = "+";
                 }
-
                 return {
                     formatted: `${prefix}${diffMinutes.toString().padStart(2, '0')}:${diffSeconds.padStart(6, '0')}`,
                     raw: rawDiffInSeconds
                 };
             };
-
             const { formatted: timeDifference, raw: rawTimeDiff } = solveTIME(timePB, authorTime);
-
-            // Create the timeDIV & replace the Track Number with it
             const timeDiv = $('<div>', {
                 class: 'tm-map-card-official-header time-div',
                 text: `${timeDifference}`
             });
             trackElement.find('.tm-map-card-official .tm-map-card-official-header').replaceWith(timeDiv);
-
-            // Compute the percentage difference
             const solvePERCENT = (rawDiff, authorTime) => {
                 const [minutesPercent, secondsPercent] = authorTime.split(':').map(parseFloat);
                 const totalTimeInSeconds = minutesPercent * 60 + secondsPercent;
-
                 if (timePB === "00:00.000") {
                     return "-.--%";
                 }
-
                 const percentDifference = (rawDiff / totalTimeInSeconds) * 100;
                 let prefix = rawDiff >= 0 ? "-" : "+";
                 return `${prefix}${Math.abs(percentDifference).toFixed(2)}%`;
             };
-
             const percentDifference = solvePERCENT(rawTimeDiff, authorTime);
-
-            // Create the percent-div & append after the time div
             const percentDiv = $('<div>', {
                 class: 'tm-map-card-official-header percent-div',
                 text: `${percentDifference}`
             });
             percentDiv.insertAfter(trackElement.find('.tm-map-card-official .time-div'));
-
-            // Function to determine track type and return corresponding class
             const determineTrackType = (timePB, rawTimeDiff) => {
                 if (timePB === "00:00.000") {
                     return 'unfinished-track';
@@ -120,43 +85,47 @@ ALLtracks.each(function() {
                     return 'authored-track';
                 }
             };
-            $$(`Info Appended`);
-            // Determine the track type and add the appropriate class
             const trackType = determineTrackType(timePB, rawTimeDiff);
             trackElement.addClass(trackType);
+            $$(`Info Appended`);
+        });
+};
 
+const OCtracks = $('.container.mt-5.d-block.d-xxl-none .col');
+$$(OCtracks);
+const COTDtracks = $('.container.my-2 .col.p-1');
+$$(COTDtracks);
+const ALLtracks = OCtracks.add(COTDtracks);
 
-// Store the original content immediately
+ALLtracks.each(function() {
+    processTrackElement($(this));
+});
+
+// Store the originals AFTER they have been processed
 const origTracksDIV = $('.row.g-2.row-cols-2.row-cols-sm-2.row-cols-md-3.row-cols-lg-4.row-cols-xl-5').clone();
+$$(`original tracks stored`);
 
-// Toggle button class
-$('#allTracksBTN').off('click').on('click', function() {
+$('body').on('click', '#allTracksBTN', function() {
     $(this).toggleClass('btn-primary btn-secondary');
-
+    $$(`button class toggled`);
     if ($(this).hasClass('btn-secondary')) {
         $.get('https://www.trackmania.com/campaigns', function(data) {
             $$(`fetching`);
-            var fetchedPage = $(data);
-            var fetchedTracks = fetchedPage.find('.col.p-1');
+            const fetchedPage = $(data);
+            const fetchedTracks = fetchedPage.find('.col.p-1');
             $$(`Fetched`);
-
-            // Replace the original div with fetched tracks and give it a unique ID
             const allTracksDIV = $('<div>', {
                 class: 'row g-2 row-cols-2 row-cols-sm-2 row-cols-md-3 row-cols-lg-4 row-cols-xl-5',
                 id: 'fetchedTracksContainer'
             }).append(fetchedTracks);
             $('.row.g-2.row-cols-2.row-cols-sm-2.row-cols-md-3.row-cols-lg-4.row-cols-xl-5').replaceWith(allTracksDIV);
+            fetchedTracks.each(function() {
+                processTrackElement($(this));
+            });
             $$(`Old Tracks Replaced with New Tracks`);
         });
     } else {
-        // When toggled off (show original content)
         $('#fetchedTracksContainer').replaceWith(origTracksDIV);
         $$(`Reverted to Original Tracks`);
     }
-});
-
-
-
-        });
-
 });
